a18bb0369c5e34bac6df20c482b2088f8d9e5fde
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 19:54:50 +0200

    Installed react-query lib.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,25 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const defaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    retry: 0,
+  },
+};
+
+const queryClient = new QueryClient({
+  defaultOptions,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
