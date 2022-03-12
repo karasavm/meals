8b35e16f7c7f2a87551d93b87e8091257394eda3
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 20:19:12 +0200

    Added button to navigate between pages.

--- src/pages/MealsPage.tsx
+++ src/pages/MealsPage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import MealItemList from "../components/MealItemList";
-import { Box, Input } from "@chakra-ui/react";
+import { Box, Input, Button } from "@chakra-ui/react";
 import { getMeals } from "../services/meals";
 import { useQuery } from "react-query";
 
 export default function MealsPage() {
   const [query, setQuery] = React.useState("");
+  const [page, setPage] = React.useState(0);
 
-  const { data = [] } = useQuery(["meals", query], () =>
-    getMeals({ query }).then(({ meals }) => meals.slice(0, 3))
+  const { data = [] } = useQuery(["meals", query, page], () =>
+    getMeals({ query, page }).then(({ meals, hasMore }) => meals)
   );
 
   return (
@@ -20,6 +21,18 @@ export default function MealsPage() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+      </Box>
+      <Box display="flex" justifyContent="space-between" p="2">
+        <Button
+          colorScheme="cyan"
+          disabled={page === 0}
+          onClick={() => setPage((p) => Math.max(0, p - 1))}
+        >
+          Prev
+        </Button>
+        <Button onClick={() => setPage((p) => p + 1)} colorScheme="cyan">
+          Next
+        </Button>
       </Box>
       <MealItemList meals={data} />
     </Box>
