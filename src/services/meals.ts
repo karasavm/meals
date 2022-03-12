e36503b217c3d53d8c9bd9dc88be7580f21dc0f9
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 20:18:31 +0200

    Implemented virtual paginated api.

--- src/services/meals.ts
+++ src/services/meals.ts
@@ -4,15 +4,24 @@ import { parseMeal } from "./utils";
 
 type GetMealsParams = {
   query?: string;
+  limit?: number;
+  page?: number;
 };
 
-function getMeals({ query = "" }: GetMealsParams = {}) {
+function getMeals({ query = "", limit = 3, page = 0 }: GetMealsParams = {}) {
   return axios
     .get<{ meals: MealApi[] }>(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+      `https://www.themealdb.com/api/json/v1/1/search.php?limit=20&s=${query}`
     )
     .then(({ data: { meals } }) => ({
-      meals: meals.map(parseMeal),
+      meals: meals
+        // VIRTUAL implementation of pagination, for demonstration purposes only
+        // because MEALdb does not support
+        .slice(page * limit, Math.min(page * limit + limit, meals.length))
+        .map(parseMeal),
+      page,
+      limit,
+      hasMore: page * limit + limit < meals.length,
     }));
 }
 
