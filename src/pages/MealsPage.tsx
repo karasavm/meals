3300793430b0f7f98f1c287e09d9ad57e8968457
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 19:48:47 +0200

    Get api used for meals.

--- src/pages/MealsPage.tsx
+++ src/pages/MealsPage.tsx
@@ -2,39 +2,16 @@ import MealItemList from "../components/MealItemList";
 import React from "react";
 import { Meal } from "../types";
 import { Box, Input } from "@chakra-ui/react";
+import { getMeals } from "../services/meals";
 
 export default function MealsPage() {
-  const [meals] = React.useState<Meal[]>([
-    {
-      id: "1",
-      title: "Meal 1",
-      thumb:
-        "https://www.themealdb.com/images/media/meals/yvpuuy1511797244.jpg",
-      ingredients: [
-        "potato",
-        "2 eggs",
-        "tomato",
-        "tomato",
-        "tomato",
-        "tomato",
-        "tomato",
-      ],
-    },
-    {
-      id: "2",
-      title: "Meal 1",
-      thumb:
-        "https://www.themealdb.com/images/media/meals/yvpuuy1511797244.jpg",
-      ingredients: ["potato", "2 eggs", "tomato"],
-    },
-    {
-      id: "3",
-      title: "Meal 1",
-      thumb:
-        "https://www.themealdb.com/images/media/meals/yvpuuy1511797244.jpg",
-      ingredients: ["potato", "2 eggs", "tomato"],
-    },
-  ]);
+  const [meals, setMeals] = React.useState<Meal[]>([]);
+
+  React.useEffect(() => {
+    getMeals().then(({ meals }) => {
+      setMeals(meals.slice(0, 3));
+    });
+  }, []);
 
   return (
     <Box w="70%" display="flex" flexDirection="column">
