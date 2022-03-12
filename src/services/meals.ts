f3311b1b1b6215010e7594336f16574862f2dc9a
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 22:17:09 +0200

    Fix when meals is null from api.

--- src/services/meals.ts
+++ src/services/meals.ts
@@ -9,21 +9,35 @@ type GetMealsParams = {
 };
 
 function getMeals({ query = "", limit = 3, page = 0 }: GetMealsParams = {}) {
-  return axios
-    .get<{ meals: MealApi[] }>(
-      `https://www.themealdb.com/api/json/v1/1/search.php?limit=20&s=${query}`
-    )
-    .then(({ data: { meals } }) => ({
-      meals: meals
-        // VIRTUAL implementation of pagination, for demonstration purposes only
-        // because MEALdb does not support
-        .slice(page * limit, Math.min(page * limit + limit, meals.length))
-        .map(parseMeal),
-      page,
-      limit,
-      hasMore: page * limit + limit < meals.length,
-      size: Math.ceil(meals.length / limit),
-    }));
+  return (
+    axios
+      .get<{ meals: MealApi[] }>(
+        `https://www.themealdb.com/api/json/v1/1/search.php?limit=20&s=${query}`
+      )
+      // Return empty array when meals is null
+      .then((data) =>
+        data.data.meals === null
+          ? {
+              ...data,
+              data: {
+                ...data.data,
+                meals: [],
+              },
+            }
+          : data
+      )
+      .then(({ data: { meals } }) => ({
+        meals: meals
+          // VIRTUAL implementation of pagination, for demonstration purposes only
+          // because MEALdb does not support
+          .slice(page * limit, Math.min(page * limit + limit, meals.length))
+          .map(parseMeal),
+        page,
+        limit,
+        hasMore: page * limit + limit < meals.length,
+        size: Math.ceil(meals.length / limit),
+      }))
+  );
 }
 
 export { getMeals };
