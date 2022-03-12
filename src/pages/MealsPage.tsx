4edfb4694fa9e15880c9759c22f23e056fe6cc5d
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 20:01:33 +0200

    Minor style fix.

--- src/pages/MealsPage.tsx
+++ src/pages/MealsPage.tsx
@@ -1,16 +1,25 @@
+import React from "react";
 import MealItemList from "../components/MealItemList";
 import { Box, Input } from "@chakra-ui/react";
 import { getMeals } from "../services/meals";
 import { useQuery } from "react-query";
+
 export default function MealsPage() {
-  const { data = [] } = useQuery(["meals"], () =>
-    getMeals().then(({ meals }) => meals.slice(0, 3))
+  const [query, setQuery] = React.useState("");
+
+  const { data = [] } = useQuery(["meals", query], () =>
+    getMeals({ query }).then(({ meals }) => meals.slice(0, 3))
   );
 
   return (
-    <Box w="70%" display="flex" flexDirection="column">
+    <Box w="70%" display="flex" flexDirection="column" flex={1}>
       <Box p="2">
-        <Input type="text" placeholder="Search..." />
+        <Input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
       </Box>
       <MealItemList meals={data} />
     </Box>
