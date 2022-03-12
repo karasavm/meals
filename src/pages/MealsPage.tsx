ae401ebfdfbe9307647d58d8036a7a1eee4aa324
Michalis Karasavvas <karasavm@gmail.com>
2022-03-12 19:55:14 +0200

    Manual fetch replaced by react-query

--- src/pages/MealsPage.tsx
+++ src/pages/MealsPage.tsx
@@ -1,24 +1,18 @@
 import MealItemList from "../components/MealItemList";
-import React from "react";
-import { Meal } from "../types";
 import { Box, Input } from "@chakra-ui/react";
 import { getMeals } from "../services/meals";
-
+import { useQuery } from "react-query";
 export default function MealsPage() {
-  const [meals, setMeals] = React.useState<Meal[]>([]);
-
-  React.useEffect(() => {
-    getMeals().then(({ meals }) => {
-      setMeals(meals.slice(0, 3));
-    });
-  }, []);
+  const { data = [] } = useQuery(["meals"], () =>
+    getMeals().then(({ meals }) => meals.slice(0, 3))
+  );
 
   return (
     <Box w="70%" display="flex" flexDirection="column">
       <Box p="2">
         <Input type="text" placeholder="Search..." />
       </Box>
-      <MealItemList meals={meals} />
+      <MealItemList meals={data} />
     </Box>
   );
 }
